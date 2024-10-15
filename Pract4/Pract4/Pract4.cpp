51f7044051faf199afdf260a1d7af5f73a0a68e2
Kotorama <michael.shapovalov3012@gmail.com>
2024-10-15 14:12:51 +0300

    commit

--- Pract4/Pract4/Pract4.cpp
+++ Pract4/Pract4/Pract4.cpp
@@ -1,29 +1,45 @@
-//14. Сформувати документ, в якому зберігається інформація про
-//асортимент продовольчих товарів в магазині : назва магазину, код товару,
-//кількість цього товару, ціна за кілограм.Відсортувати магазини для оптової
-//закупки заданого товару згідно його ціни.
+п»ї//14. РЎС„РѕСЂРјСѓРІР°С‚Рё РґРѕРєСѓРјРµРЅС‚, РІ СЏРєРѕРјСѓ Р·Р±РµСЂС–РіР°С”С‚СЊСЃСЏ С–РЅС„РѕСЂРјР°С†С–СЏ РїСЂРѕ
+//Р°СЃРѕСЂС‚РёРјРµРЅС‚ РїСЂРѕРґРѕРІРѕР»СЊС‡РёС… С‚РѕРІР°СЂС–РІ РІ РјР°РіР°Р·РёРЅС– : РЅР°Р·РІР° РјР°РіР°Р·РёРЅСѓ, РєРѕРґ С‚РѕРІР°СЂСѓ,
+//РєС–Р»СЊРєС–СЃС‚СЊ С†СЊРѕРіРѕ С‚РѕРІР°СЂСѓ, С†С–РЅР° Р·Р° РєС–Р»РѕРіСЂР°Рј.Р’С–РґСЃРѕСЂС‚СѓРІР°С‚Рё РјР°РіР°Р·РёРЅРё РґР»СЏ РѕРїС‚РѕРІРѕС—
+//Р·Р°РєСѓРїРєРё Р·Р°РґР°РЅРѕРіРѕ С‚РѕРІР°СЂСѓ Р·РіС–РґРЅРѕ Р№РѕРіРѕ С†С–РЅРё
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+const int n = 3;
+struct Product {
+    string productName;
+    int amount;
+    int cost;
+};
+
+struct Shop {
+    string shopName;
+    vector <Product> products;
+};
+
+string priceSort(vector <Shop> shopVector, string product) {
+    int min = 0;
+    string name;
+    for (int i = 0; i < shopVector.size(); i++) {
+        for (int j = 0; j < shopVector.at(i).products.size(); j++) {
+            if (shopVector.at(i).products.at(j).cost < min || min == 0) {
+                min = shopVector.at(i).products.at(j).cost;
+                name = shopVector.at(i).shopName;
+            }
+        }
+    }
+    return name;
+}
+
 int main()
 {
-    const int n = 3;
-    struct Product {
-        string productName;
-        int amount;
-        int cost;
-    };
+    vector <Shop> Marketplace = { 
+        { "Shop1", { {"Product1", 2, 6}, {"Product2", 2, 6}, {"Product3", 0, 6} } },
+        { "Shop2", { {"Product1", 2, 9}, {"Product2", 0, 8}, {"Product3", 2, 7} } },
+        { "Shop3", { {"Product1", 0, 8}, {"Product2", 2, 5}, {"Product3", 2, 2} } } };
 
-    struct Shop {
-        string shopName;
-        Product products[3];
-    };
-
-    {
-        Shop Marketplace[3] = { 
-            { "Shop1", { {"Product1", 2, 6}, {"Product2", 2, 6}, {"Product3", 2, 6} } },
-            { "Shop2", { {"Product1", 2, 9}, {"Product2", 2, 8}, {"Product3", 2, 7} } },
-            { "Shop3", { {"Product1", 2, 8}, {"Product2", 2, 5}, {"Product3", 2, 2} } } };
-
+    cout << priceSort(Marketplace, "Product3");
 }
